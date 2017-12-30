fbea0d789f7a561b878aced9876ce2d7602c253e
park-alter <pakrulizwan@yahoo.com>
2017-12-30 10:08:38 +0800

    update version v1.0.0-beta.2
    + remove electron-rebuild deps

--- bin/build.js
+++ bin/build.js
@@ -33,78 +33,20 @@ let options = {
     app: opts.dir || './node_modules/wmcc-desktop'
   },
   electronVersion: opts.electronVersion || '1.7.9',
-  npmRebuild: false
+  npmRebuild: JSON.parse(opts.rebuild) || true
 }
 
 options = assignOptions(options, opts.platform, opts.arch);
 
-if (opts.rebuild === "true") {
-  let spinner;
-  let packages = [
-    'node-x15',
-    'leveldown',
-    'secp256k1'/*,
-    'wmcc-native'*/
-  ];
-
-  _rebuild();
-  
-  console.log(`Please wait, this may take several moments`);
-  function _rebuild() {
-    spinner = _spinner(`Installing ${packages[0]}`);
-    const child = Child.exec(`cd ./node_modules/${packages[0]} && npm install --ignore-scripts`);
-
-    child.on('exit', ()=>{
-      clearInterval(spinner);
-      spinner = _spinner(`Rebuild native module: ${packages[0]}`);
-      Rebuild.rebuild({
-        buildPath: Path.resolve('node_modules', packages[0]),
-        electronVersion: options.electronVersion
-      }).then(() => {
-        _clearLine(spinner);
-        console.log(`\u2714 Finished building: ${packages[0]}!`);
-        packages.shift();
-        if (packages.length)
-          _rebuild();
-        else
-          _build();
-      }).catch((e) => {
-        _clearLine();
-        console.error(e);
-      });
-    });
-  }
-} else {
-  _build();
-}
-
-function _build() {
-  Builder.build({
-    config: options
-  }).then(() => {
-    console.log(`\u2714 Successfully compiled!`);
-    process.exit(0);
-  }).catch((e) => {
-    console.error(e);
-    process.exit(-1);
-  });
-}
-
-function _spinner(t) {
-  let x = 0;
-  const f = ["- ----","-- ---","--- --","---- -","----- ","---- -","--- --","-- ---","- ----"," -----"];
-  return setInterval(()=>{
-    x = (x>f.length-1)?0:x;
-    process.stdout.write(`\r${f[x]} ${t} ${f[x]}`);
-    x++;
-  }, 150);
-}
-
-function _clearLine (o) {
-  if (o) clearInterval(o);
-  process.stdout.clearLine();
-  process.stdout.write(`\r`);
-}
+Builder.build({
+  config: options
+}).then(() => {
+  console.log(`\u2714 Successfully compiled!`);
+  process.exit(0);
+}).catch((e) => {
+  console.error(e);
+  process.exit(-1);
+});
 
 /** helper */
 function assignOptions(opts, platform, arch) {
